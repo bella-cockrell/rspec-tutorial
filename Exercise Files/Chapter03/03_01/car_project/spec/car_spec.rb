e18f48f52ceda628b2d93f84ddf45a515e0eb6ce
bella-cockrell <arabella.cockrell@gmail.com>
2020-09-29 21:12:51 +0100

    Adding new test and pending test

--- Exercise Files/Chapter03/03_01/car_project/spec/car_spec.rb
+++ Exercise Files/Chapter03/03_01/car_project/spec/car_spec.rb
@@ -1,5 +1,12 @@
 require 'car' 
 
 describe 'Car' do
-     
+    context 'attributes' do
+        it "allows reading and writing for :make" do
+            car = Car.new
+            car.make = "Test"
+            expect(car.make).to eq("Test")
+        end
+        it "allows writing for :doors"
+    end
 end
